a069f14a4f71a82114005b704577c3468cc6a0b0
Harrisam <samantha.s.harris@googlemail.com>
2013-09-26 18:52:39 +0100

    Data_mapper set up

--- app/bookmarkmanager.rb
+++ app/bookmarkmanager.rb
@@ -2,20 +2,9 @@ require 'sinatra/base'
 require 'haml'
 require 'data_mapper'
 require 'rack-flash'
-
-env = ENV["RACK_ENV"] || "development"
-#we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" depending on the environment
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-
 require './lib/link' #this needs to be done after datamapper is initialised
 require './lib/tag'
 require './lib/user'
-
-#after declaring you models, you should finalise them
-DataMapper.finalize
-
-#however, how database tables don't exist yet. Let's tell datamapper to creat them.
-DataMapper.auto_upgrade!
 
 require_relative 'helpers/application'
 
---
+++ app/data_mapper_setup.rb
@@ -0,0 +1,6 @@
+env = ENV["RACK_ENV"] || "development"
+#we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" depending on the environment
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
